52a16ef2d84810efdad5e1f994034346ebf074c7
catkidd <catkidd770@gmail.com>
2023-04-14 06:42:14 +0545

    fix: code link updated in projectdetail page

--- pages/projectOne.jsx
+++ pages/projectOne.jsx
@@ -65,7 +65,10 @@ const projectOne = () => {
                     </div>
 
                     <div className="flex flex-row justify-items-start">
-                        <a href="/" target="_blank" rel="noreferrer">
+                        <a
+                            href="https://github.com/catkidd/portfolio-nextjs"
+                            target="_blank"
+                            rel="noreferrer">
                             <button className="px-8 py-2 mt-4 mr-8">Code</button>
                         </a>
                         <a href="/" target="_blank" rel="noreferrer">
