9d155f9d8d0e8d8030a84e4042ce2a244445b6fc
catkidd <catkidd770@gmail.com>
2023-04-19 05:59:15 +0545

    fix: toaster error fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App({ Component, pageProps }) {
     return (
         <>
             <Navbar />
             <Component {...pageProps} />
+            <ToastContainer />
         </>
     );
 }
