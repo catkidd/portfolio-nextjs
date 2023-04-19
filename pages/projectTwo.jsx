307213342a84a9b6b634658b0c65c45af39240b9
catkidd <catkidd770@gmail.com>
2023-04-19 13:45:40 +0545

    fix: link updated in code button for weather app

--- pages/projectTwo.jsx
+++ pages/projectTwo.jsx
@@ -68,7 +68,10 @@ const projectTwo = () => {
                         </p>
                     </div>
                     <div className="flex flex-row justify-items-start">
-                        <a href="/" target="_blank" rel="noreferrer">
+                        <a
+                            href="https://github.com/catkidd/weather-app"
+                            target="_blank"
+                            rel="noreferrer">
                             <button className="px-8 py-2 mt-4 mr-8">Code</button>
                         </a>
                         <a
